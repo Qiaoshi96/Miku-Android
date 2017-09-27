ebf54d373588e57102d2221c508711fd7efc2cae
Cuizhen <821691846@qq.com>
2017-09-27 11:28:26 +0800

    网易云通信SDK集成

--- app/src/main/java/com/miku/ktv/miku_android/view/activity/KTVActivity.java
+++ app/src/main/java/com/miku/ktv/miku_android/view/activity/KTVActivity.java
@@ -5,11 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.miku.ktv.miku_android.R;
+import com.netease.nimlib.sdk.avchat.AVChatManager;
+import com.netease.nimlib.sdk.avchat.constant.AVChatVideoScalingType;
+import com.netease.nimlib.sdk.avchat.model.AVChatCameraCapturer;
+import com.netease.nimlib.sdk.avchat.model.AVChatSurfaceViewRenderer;
+import com.netease.nimlib.sdk.avchat.model.AVChatVideoCapturerFactory;
 
 public class KTVActivity extends AppCompatActivity  implements View.OnClickListener{
 
@@ -18,6 +25,13 @@ public class KTVActivity extends AppCompatActivity  implements View.OnClickListe
     private LinearLayout mLlDiangelist;
     private ImageView mIvmore;
 
+    private AVChatCameraCapturer videoCapturer; // 视频采集模块
+    AVChatSurfaceViewRenderer masterRender; // 自己的画布
+
+    private ViewGroup MyVideoLayout;
+    private ImageView mIvVideo;
+
+    private boolean isOpen = false ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +48,7 @@ public class KTVActivity extends AppCompatActivity  implements View.OnClickListe
         mLlDiangelist.setOnClickListener(this);
         mLlPaimailist.setOnClickListener(this);
         mIvmore.setOnClickListener(this);
+        mIvVideo.setOnClickListener(this);
     }
     private void initView() {
         //返回键的控件
@@ -44,6 +59,10 @@ public class KTVActivity extends AppCompatActivity  implements View.OnClickListe
         mLlDiangelist = (LinearLayout) findViewById(R.id.ll_diangelist);
         //更多按钮
         mIvmore = (ImageView) findViewById(R.id.iv_more);
+        //开启摄像头按钮
+        mIvVideo = (ImageView) findViewById(R.id.iv_video);
+        //第一个摄像头区域
+        MyVideoLayout = (ViewGroup) findViewById(R.id.v1);
     }
 
     @Override
@@ -64,8 +83,54 @@ public class KTVActivity extends AppCompatActivity  implements View.OnClickListe
             case R.id.iv_more:
                 showDialog();
                 break;
+            case R.id.iv_video:
+
+               /* if (isOpen = false){
+                openVideo();
+                    isOpen = !isOpen;
+                }else{
+                    closeVideo();
+                    isOpen = !isOpen;
+                }*/
+               openVideo();
+                break;
 
         }
+    }
+
+    private void closeVideo() {
+        //关闭视频预览
+        AVChatManager.getInstance().stopVideoPreview();
+        //关闭音视频引擎
+        AVChatManager.getInstance().disableRtc();
+    }
+
+    private void openVideo() {
+        AVChatManager.getInstance().enableRtc();
+        // 打开视频模块
+        AVChatManager.getInstance().enableVideo();
+        // 设置视频采集模块
+        if (videoCapturer == null) {
+            videoCapturer = AVChatVideoCapturerFactory.createCameraCapturer();
+            AVChatManager.getInstance().setupVideoCapturer(videoCapturer);
+        }
+        //
+        if (masterRender == null) {
+            masterRender = new AVChatSurfaceViewRenderer(this);
+        }
+        addIntoMasterPreviewLayout(masterRender);
+        AVChatManager.getInstance().setupLocalVideoRender(masterRender, false, AVChatVideoScalingType.SCALE_ASPECT_BALANCED);
+        //打开视频预览
+        AVChatManager.getInstance().startVideoPreview();
+
+    }
+    //把自己添加到画布
+    private void addIntoMasterPreviewLayout(SurfaceView surfaceView) {
+        if (surfaceView.getParent()!=null)
+            ((ViewGroup) surfaceView.getParent()).removeView(surfaceView);
+            MyVideoLayout.addView(surfaceView);
+        surfaceView.setZOrderMediaOverlay(true);
+
     }
 
     private void showDialog() {
